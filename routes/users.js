9df72b3afd5d22b06675223f459b063714cf9296
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-21 20:45:02 +0700

    create user with uuid

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
-];
+const users = [];
 
 router.get("/", (req, res) => {
   res.json(users);
@@ -22,7 +12,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const user = req.body;
 
-  users.push(user);
+  users.push({ id: uuidv4(), ...user });
 
   res.json(`User with the name ${user.firstName} added to the database! `);
 });
