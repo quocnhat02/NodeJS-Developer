b25cbf1241b8553ae48b2aac387287c2a453bed8
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-21 20:37:11 +0700

    create user

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,23 @@ const users = [
     lastName: "Doe",
     age: 25,
   },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 24,
+  },
 ];
 
 router.get("/", (req, res) => {
   res.json(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+
+  res.json(`User with the name ${user.firstName} added to the database! `);
 });
 
 export default router;
