163e80950bba62789cdfb87eaca50084acbb8ed6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-29 19:54:12 +0700

    exporting from modules

--- https.js
+++ https.js
@@ -1,4 +1,10 @@
-function request(url, data) {
+const { send } = require("./request");
+const { read } = require("./response");
+
+function makeRequest(url, data) {
   send(url, data);
   return read();
 }
+
+const responseData = makeRequest("https://google.com", "Hello");
+console.log(responseData);
--- request.js
+++ request.js
@@ -1,3 +1,5 @@
+const REQUEST_TIMEOUT = 500;
+
 function encrypt(data) {
   return "encrypted data";
 }
@@ -6,3 +8,8 @@ function send(url, data) {
   const encryptedData = encrypt(data);
   console.log(`Sending ${encryptedData} to ${url}`);
 }
+
+module.exports = {
+  REQUEST_TIMEOUT,
+  send,
+};
