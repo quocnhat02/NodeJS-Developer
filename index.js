bc0ee4bb0f5a7e55acf9728c48863b743c30a172
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-30 11:15:13 +0700

    parameterized urls

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: "Nicola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einstein",
+  },
+];
+
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
-    // res.writeHead(200, {
-    //   "Content-Type": "application/json",
-    // });
+  const items = req.url.split("/");
+
+  if (items[1] === "/friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        id: 1,
-        name: "Sir Isaac Newton",
-      })
-    );
-  } else if (req.url === "/messages") {
+    res.end(JSON.stringify());
+  } else if (items[1] === "messages") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
