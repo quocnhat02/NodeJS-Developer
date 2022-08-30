20b8a3c46e1c9b151c941b9cf9b8d2882d8a7451
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-30 22:33:26 +0700

    route parameters

--- server.js
+++ server.js
@@ -4,15 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+const friends = [
+  {
+    id: 0,
+    name: "Albert Einstein",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+// GET /friends/22
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
-  res.send({
-    id: 1,
-    name: "Sir Isaac Newton",
-  });
+  res.send("<ul><li>Hello Albert</li></ul>");
 });
 
 app.post("/messages", (req, res) => {
