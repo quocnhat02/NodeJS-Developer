96db0faf5faa8d4f875ea8e1be92d7a2f68ef07e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-30 09:47:37 +0700

    exploring habitable planets

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanet = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
   .pipe(
     parse({
-      Comment: "#",
+      comment: "#",
+      relax_column_count: true,
       columns: true,
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanet.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
+    console.log(
+      habitablePlanet.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlanet.length} habitable planets found!`);
     console.log("done");
   });
 
