040bb7808b8b9c9332a41809758338d882497782
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-02 17:46:29 +0700

    logging request with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
@@ -11,6 +12,9 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req, res) => {
