3b9e1c4b2f900071b13ccf2d3d4648063ecf43d5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 22:52:46 +0700

    webserver 3

--- http-server/index.js
+++ http-server/index.js
@@ -5,16 +5,30 @@ const PORT = 3000;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "Sir Isaac Newton",
-    })
-  );
+  if (req.url === "/friends") {
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    // });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: "Sir Isaac Newton",
+      })
+    );
+  } else if (req.url === "/message") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac</li>");
+    res.write("<li>What are your thoughts on astronomy?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
