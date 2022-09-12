86fa53ff88d5ebbe4168e48dc7c8efb39f082e69
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 21:16:51 +0700

    finding habitable planets 3

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
     planet["koi_insol"] > 0.36 &&
-    planet["koi_insol"] < 1.11
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
   );
 }
 
