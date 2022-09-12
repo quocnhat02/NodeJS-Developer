3e3724fb0cc87706f110f21f555a39dd599f08dd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 23:48:37 +0700

    webserver 9

--- http-server/index.js
+++ http-server/index.js
@@ -27,6 +27,7 @@ server.on("request", (req, res) => {
       const friend = data.toString();
       console.log("Request: ", friend);
       friends.push(JSON.parse(friend));
+      req.pipe(res);
     });
   } else if (req.method === "GET" && items[1] === "friends") {
     res.statusCode = 200;
