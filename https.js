b97c74dcf4f0c06d9da8c48ce23df2ddc30fef09
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 18:20:16 +0700

    using index.js

--- https.js
+++ https.js
@@ -1,5 +1,5 @@
-const { send } = require("./request");
-const { read } = require("./response");
+const { send } = require("./internals/request");
+const { read } = require("./internals/response");
 
 function makeRequest(url, data) {
   send(url, data);
