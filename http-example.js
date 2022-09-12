8798f95c3672f750c31584860cff78b9673f7eab
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 17:17:53 +0700

    making http request 7

--- http-example.js
+++ http-example.js
@@ -1,6 +1,6 @@
-const https = require("https");
+const { request } = require("https");
 
-const req = https.request("https://www.google.com", (res) => {
+const req = request("https://www.google.com", (res) => {
   res.on("data", (chunk) => {
     console.log(`Data chunk: ${chunk}`);
   });
