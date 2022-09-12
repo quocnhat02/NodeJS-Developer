494315e1ef7c4cffa0c19f90d5147ad69d22af92
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 17:12:20 +0700

    making http request 3

--- http-example.js
+++ http-example.js
@@ -1,3 +1,7 @@
 const http = require("http");
 
-http.request("http://www.google.com", (res) => {});
+http.request("http://www.google.com", (res) => {
+  res.on("data", (chunk) => {
+    console.log(`Data chunk: ${chunk}`);
+  });
+});
