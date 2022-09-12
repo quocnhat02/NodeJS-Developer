7c1464c3b9c2b0bcd5dd2c1a497c290efd2cc173
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 18:27:31 +0700

    using index.js 4

--- https.js
+++ https.js
@@ -1,10 +1,10 @@
-const internals = require("./internals");
+const { send, read } = require("./internals");
 // const { send } = require("./internals/request");
 // const { read } = require("./internals/response");
 
 function makeRequest(url, data) {
-  internals.send(url, data);
-  return internals.read();
+  send(url, data);
+  return read();
 }
 
 const responseData = makeRequest("https://google.com", "hello");
