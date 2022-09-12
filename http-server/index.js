d8a2bf8236affa29e1f6d1e73b4a7208fa054059
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-12 22:43:22 +0700

    webserver 1

--- http-server/index.js
+++ http-server/index.js
@@ -4,10 +4,15 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    "Content-Type": "text/plain",
+    "Content-Type": "application/json",
   });
 
-  res.end("Hello! Sir Issac Newton is your friend!");
+  res.end(
+    JSON.stringify({
+      id: 1,
+      name: "Sir Isaac Newton",
+    })
+  );
 });
 
 server.listen(PORT, () => {
