172cd02f409dd1671c59a6dd08fd2bbaa2cbb61d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-14 00:26:34 +0700

    automating full stack app with npm 3

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
