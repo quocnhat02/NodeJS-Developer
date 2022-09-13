fccb31fce2336e60a2883297b2d32eb773cc31c4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-13 21:15:42 +0700

    api server setup 9

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
+const planetsRouter = require("./routes/planets/planets.router");
+
 const app = express();
 
+// MIDDLEWARE
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+// ROUTES
+app.use(planetsRouter);
 
 module.exports = app;
