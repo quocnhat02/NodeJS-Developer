6d3eeab4e1f92d2e6d08093f558a15ef632b1329
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-14 08:35:31 +0700

    morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -12,6 +13,7 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(
   express.urlencoded({
