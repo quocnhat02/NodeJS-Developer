d540935e278fd018559709f35d6c959326512916
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-14 13:51:11 +0700

    testing apis endpoints with supertest GET 4

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,7 +11,18 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launch", () => {
-  test("It should respond with 200 success", () => {});
+  test("It should respond with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "Kepler-186 f",
+        launchDate: "January 4, 2028",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201);
+  });
 
   test("It should catch missing required properties", () => {});
   test("It should catch missing required dates", () => {});
