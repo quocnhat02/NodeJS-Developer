0b2266219e8ca225805e124604ca60d60d209838
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-14 10:53:40 +0700

    post launches creating launches 1

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -15,6 +17,16 @@ launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
   return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+    })
+  );
 }
 
 module.exports = {
