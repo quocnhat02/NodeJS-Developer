36a83de63c5ea69626d2de7027d3a045ca8f5431
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-14 10:28:47 +0700

    serving app with csr

--- server/src/app.js
+++ server/src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 // ROUTES
 app.use(planetsRouter);
 app.use(launchesRouter);
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
