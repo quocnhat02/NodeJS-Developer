e41df0bb228137fd9c6edd3429cf45e0dc3113f8
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-14 13:46:09 +0700

    testing apis endpoints with supertest GET 2

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const app = require("../../app");
 
 describe("Test GET /launches", () => {
-  test("It should respond with 200 success", () => {
-    const response = request(app);
-    expect(response).toBe(200);
+  test("It should respond with 200 success", async () => {
+    const response = await request(app).get("/launches");
+    expect(response.statusCode).toBe(200);
   });
 });
 
