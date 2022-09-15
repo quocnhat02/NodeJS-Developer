f40bb4e6694a861f9c92a64a5b47286ec66ffd53
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-15 21:30:50 +0700

    updating tests for mongoose 1

--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,16 @@
 const http = require("http");
-const mongoose = require("mongoose");
-
 const app = require("./app");
+
+const { mongoConnect } = require("./services/mongo");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = "mongodb://localhost:27017/nasa-api";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
---
+++ server/src/services/mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://localhost:27017/nasa-api";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+  });
+}
+
+module.exports = {
+  mongoConnect,
+};
