ac0fe2b3d6172747d64af8ecdef1678555c0a77a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-16 11:43:55 +0700

    setting up our security example 7

--- security/server.js
+++ security/server.js
@@ -1,5 +1,6 @@
-const https = require("https");
+const fs = require("fs");
 const path = require("path");
+const https = require("https");
 const express = require("express");
 
 const PORT = 5000;
@@ -15,10 +16,13 @@ app.get("/", (req, res) => {
 });
 
 https
-  .createServer({
-    key: "",
-    cert: "",
-  })
+  .createServer(
+    {
+      key: fs.readFileSync("../key.pem"),
+      cert: fs.readFileSync("../cert.pem"),
+    },
+    app
+  )
   .listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
