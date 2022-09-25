e61d52c8e267b861d27ca46ffd100a16461bca8e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-25 19:30:46 +0700

    rest api

--- router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'https://deno.land/x/oak/mod.ts';
-
-const router = new Router();
-
-router.get('/', ({ response }) => {
-  response.body = 'Todo List API using Deno Runtime';
-});
-
-export default router;
