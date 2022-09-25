0275155e2d5508358d6b5a6a57a64b9ec4a86d84
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-25 19:39:13 +0700

    rest api 3

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  console.log('[TEST]!');
+
+  return res.send('Hello from Homepage');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: http://localhost:${PORT}`);
 });
