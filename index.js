ea6a005f5d7de89b8ddf635ca9b8781d79849ed7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-25 18:31:33 +0700

    deno 2

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
 
 import { PORT } from './config.js';
+import router from './router.js';
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Server is running. Open https://localhost:${PORT}`);
+
+await app.listen({ port: PORT });
