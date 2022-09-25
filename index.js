d800cc6c3548a93c898a0bf8197ca6d60a4ea31e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-25 19:45:26 +0700

    rest api 6

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 
 const PORT = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => res.send('Hello from Homepage'));
 
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
   return res.send('Hello');
 });
 
