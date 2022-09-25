8c4b3e3d335d5f92d04cb65b16d61f8a73ec3e13
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-25 20:01:50 +0700

    rest api 13

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import { v4 as uuidv4 } from 'uuid';
+
 const router = express.Router();
 
 const users = [
@@ -22,7 +24,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const user = req.body;
 
-  users.push(user);
+  users.push({ ...user, id: uuidv4() });
 
   return res.send(
     `User with the username ${user.firstName} added to the database!`
