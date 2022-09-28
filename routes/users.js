fd40688fc13173acec44785a7af55e005d284b76
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 14:40:20 +0700

    rest api 24

--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,23 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [];
+router.get('/', getUsers);
 
-router.get('/', (req, res) => {
-  return res.send(users);
-});
+router.post('/', createUser);
 
-router.post('/', (req, res) => {
-  const user = req.body;
+router.get('/:id', getUser);
 
-  users.push({ ...user, id: uuidv4() });
+router.delete('/:id', deleteUser);
 
-  return res.send(
-    `User with the username ${user.firstName} added to the database`
-  );
-});
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
-  if (!foundUser) {
-    return res.send('Not found user');
-  }
-  return res.send(foundUser);
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const foundUser = users.find((user) => user.id === id);
-
-  if (!foundUser) {
-    return res.send('Not found user');
-  }
-  users = users.filter((user) => user.id !== id);
-
-  return res.send(`User with the id ${id} deleted from the database`);
-});
+router.patch('/:id', updateUser);
 
 export default router;
