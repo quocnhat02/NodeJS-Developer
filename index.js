52cb059db8bdf82f0c6e71617e83b3dcbe3f3089
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 13:40:13 +0700

    rest api 11

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => res.send('Hello from HomePage'));
 
