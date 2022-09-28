54a75a16253899aa9fa802cde0796dea20f96220
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 13:50:21 +0700

    rest api 13

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+  },
+];
+
 router.get('/', (req, res) => {
+  console.log(users);
+
   res.send('Hello');
 });
 
