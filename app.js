b04ef756e7c9e59323c0ac2ae28106650b1eb848
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-27 00:20:47 +0700

    basic route 3

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
