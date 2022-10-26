2c739121bb9e13851c058727a20266c245f41ee1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-27 00:16:36 +0700

    basic route 2

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
 
 app.listen(PORT, () => {
