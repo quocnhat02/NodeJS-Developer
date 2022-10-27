06b40e0baea66246ffc826fcb0b2b150192c5dcd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-27 13:15:52 +0700

    get request 5

--- app.js
+++ app.js
@@ -26,23 +26,25 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
       tours,
     },
-  });
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
   });
 });
 
