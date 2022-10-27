1f62674cc72bac4321dcbe3864e22c3197b15c81
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-27 13:59:31 +0700

    refactoring our routes 2

--- app.js
+++ app.js
@@ -99,11 +99,18 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours/:id', getTour);
-app.get('/api/v1/tours', getAllTours);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
