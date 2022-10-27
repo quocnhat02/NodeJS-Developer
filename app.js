3df3a436993f25e90563208e80e91627560b5899
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-27 13:49:41 +0700

    delete request

--- app.js
+++ app.js
@@ -97,6 +97,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
