fd416eeb50721fc67cbc93c3048fe5cef1815fa2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-28 14:20:05 +0700

    import development data 2

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -40,4 +40,8 @@ const deleteData = async () => {
   }
 };
 
-console.log(process.argv);
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
