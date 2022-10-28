d140fbe495a50c92487afb9f29db9aa123adb17e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-28 13:29:58 +0700

    deleting documents 1

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -82,11 +82,20 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    return res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 module.exports = {
