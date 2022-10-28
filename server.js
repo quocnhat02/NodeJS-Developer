a6911833392e4333af2c8218a4b116967d05f522
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-28 11:42:43 +0700

    refactoring for MVC 1

--- server.js
+++ server.js
@@ -15,39 +15,6 @@ mongoose
     console.log('DB connection successful!');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  rating: 4.5,
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR:', err);
-  });
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
