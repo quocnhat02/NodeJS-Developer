08070dd0377af7c2e8b2df08c7b4e95e7c50d829
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-29 09:59:52 +0700

    pagination 2

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -58,6 +58,13 @@ const getAllTours = async (req, res) => {
 
     query = query.skip(skip).limit(limit);
 
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
