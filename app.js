2df4ec01e19b5de184777794be8fe988457a7891
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-30 22:36:26 +0700

    handling unhandled routes

--- app.js
+++ app.js
@@ -19,13 +19,15 @@ app.use(
 );
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
 // 2.Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  return res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
